fad50168fdd3d2272546fb6d2c811bfe243c03d6
Evan-Leon <evan.leon.j@gmail.com>
2021-04-22 17:33:24 -0400

    Added disabled features and other text to button

--- ajax_twitter/app/assets/javascripts/bundle.js
+++ ajax_twitter/app/assets/javascripts/bundle.js
@@ -11,6 +11,7 @@ const twitterAPIUtil = __webpack_require__(/*! ./twitter_api_util */ "./frontend
 function FollowToggle($button){
     this.userId = $button.data("user-id");
     this.initialFollowState = $button.data("initial-follow-state");
+    this.followState = this.initialFollowState;
     this.$button = $button;
     this.render();
     this.handleClick();
@@ -18,10 +19,19 @@ function FollowToggle($button){
 }
 
 FollowToggle.prototype.render = function() {
-    if (this.initialFollowState == "unfollowed") {
+
+    if (this.followState == "unfollowed") {
+        this.$button[0].disabled = false;
         this.$button.text("Follow!");
-    } else if (this.initialFollowState == "followed") {
+    } else if (this.followState == "followed") {
+        this.$button[0].disabled = false;
         this.$button.text("Unfollow!");
+    } else if (this.followState == "following"){
+        this.$button[0].disabled = true;
+        this.$button.text("following...");
+    } else if (this.followState == "unfollowing"){
+        this.$button[0].disabled = true;
+        this.$button.text("unfollowing...");
     }
 }
 
@@ -29,24 +39,29 @@ FollowToggle.prototype.handleClick = function() {
     this.$button.on("click", (e) => {
         e.preventDefault();
         // debugger
-        const successCB = response => {
-            if (this.initialFollowState == "unfollowed") {
-                this.initialFollowState = "followed";
-            } else if (this.initialFollowState == "followed") {
-                this.initialFollowState = "unfollowed";
+        const successCB = () => {
+            if (this.followState == "following") {
+                this.followState = "followed";
+            } else if (this.followState == "unfollowing") {
+                this.followState = "unfollowed";
             }
             this.render();
         }
 
-        if (this.initialFollowState == "unfollowed") {
-            twitterAPIUtil.clickFollowAjax(this.userId).then(successCB);
-            // this.initialFollowState = "followed";
-        } else if (this.initialFollowState == "followed") {
+        if (this.followState == "unfollowed") {
+            this.followState = "following";
+            this.render();
+            twitterAPIUtil.clickFollowAjax(this.userId).then((successCB));
+            
+        } else if (this.followState == "followed") {
+            this.followState = "unfollowing";
+            this.render();
             twitterAPIUtil.clickUnfollowAjax(this.userId).then(successCB);
-            // this.initialFollowState = "unfollowed";
+            
         }
         
     })
+
 }
 
 
--- ajax_twitter/frontend/follow_toggle.js
+++ ajax_twitter/frontend/follow_toggle.js
@@ -2,6 +2,7 @@ const twitterAPIUtil = require("./twitter_api_util");
 function FollowToggle($button){
     this.userId = $button.data("user-id");
     this.initialFollowState = $button.data("initial-follow-state");
+    this.followState = this.initialFollowState;
     this.$button = $button;
     this.render();
     this.handleClick();
@@ -9,10 +10,19 @@ function FollowToggle($button){
 }
 
 FollowToggle.prototype.render = function() {
-    if (this.initialFollowState == "unfollowed") {
+
+    if (this.followState == "unfollowed") {
+        this.$button[0].disabled = false;
         this.$button.text("Follow!");
-    } else if (this.initialFollowState == "followed") {
+    } else if (this.followState == "followed") {
+        this.$button[0].disabled = false;
         this.$button.text("Unfollow!");
+    } else if (this.followState == "following"){
+        this.$button[0].disabled = true;
+        this.$button.text("following...");
+    } else if (this.followState == "unfollowing"){
+        this.$button[0].disabled = true;
+        this.$button.text("unfollowing...");
     }
 }
 
@@ -21,23 +31,28 @@ FollowToggle.prototype.handleClick = function() {
         e.preventDefault();
         // debugger
         const successCB = () => {
-            if (this.initialFollowState == "unfollowed") {
-                this.initialFollowState = "followed";
-            } else if (this.initialFollowState == "followed") {
-                this.initialFollowState = "unfollowed";
+            if (this.followState == "following") {
+                this.followState = "followed";
+            } else if (this.followState == "unfollowing") {
+                this.followState = "unfollowed";
             }
             this.render();
         }
 
-        if (this.initialFollowState == "unfollowed") {
+        if (this.followState == "unfollowed") {
+            this.followState = "following";
+            this.render();
             twitterAPIUtil.clickFollowAjax(this.userId).then((successCB));
-            // this.initialFollowState = "followed";
-        } else if (this.initialFollowState == "followed") {
+            
+        } else if (this.followState == "followed") {
+            this.followState = "unfollowing";
+            this.render();
             twitterAPIUtil.clickUnfollowAjax(this.userId).then(successCB);
-            // this.initialFollowState = "unfollowed";
+            
         }
         
     })
+
 }
 
 
